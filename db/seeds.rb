d21a7e1b7bc118ea339a10d2f262af782b2d4b7c
fukurou37 <swish3578@gmail.com>
2019-07-05 00:44:39 +0900

    last

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = %w(J-POP･アジアンポップ･ポップス･ロック･ハードロック ヘヴィーメタル･ブルース カントリー･ソウル・R&B･ヒップホップ･ダンス・エレクトロニカ･ジャズ フュージョンクラシック･ワールド･サウンドトラック･ヒーリング ニューエイジ･アニメ ゲーム)
+categories = %w(J-POP アジアンポップ ポップス ロック ハードロック･ヘヴィーメタル ブルース･カントリー ソウル R&B ヒップホップ ダンス エレクトロニカ ジャズ･フュージョンクラシック ワールド サウンドトラック ヒーリング･ニューエイジ アニメ ゲーム)
 
 categories.each do |category|
   Category.create(name: category)
